1d3bb993e5b8b82864c91aaa149a6ea06bbfba6e
monicacarista <monica.carista@si.ukdw.ac.id>
2020-11-18 15:27:46 +0700

    finalll

--- protected/views/user/_form.php
+++ protected/views/user/_form.php
@@ -1,10 +1,79 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+* {
+  box-sizing: border-box;
+}
+
+input[type=text], select, textarea {
+  width: 100%;
+  padding: 12px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  resize: vertical;
+}
+
+label {
+  padding: 12px 12px 12px 0;
+  display: inline-block;
+}
+
+input[type=submit] {
+  background-color: #4CAF50;
+  color: white;
+  padding: 12px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  float: right;
+}
+
+input[type=submit]:hover {
+  background-color: #45a049;
+}
+
+.container {
+  border-radius: 5px;
+  background-color: #f2f2f2;
+  padding: 20px;
+}
+
+.col-25 {
+  float: left;
+  width: 25%;
+  margin-top: 6px;
+}
+
+.col-75 {
+  float: left;
+  width: 75%;
+  margin-top: 6px;
+}
+
+/* Clear floats after the columns */
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+/* Responsive layout - when the screen is less than 600px wide, make the two columns stack on top of each other instead of next to each other */
+@media screen and (max-width: 600px) {
+  .col-25, .col-75, input[type=submit] {
+    width: 100%;
+    margin-top: 0;
+  }
+}
+</style>
+</head>
+<body>
 <?php
-/* @var $this UserController */
-/* @var $model User */
+/* @var $this PencatatanController */
+/* @var $model Pencatatan */
 /* @var $form CActiveForm */
 ?>
-
-<div class="form">
 
 <?php $form=$this->beginWidget('CActiveForm', array(
 	'id'=>'user-form',
@@ -20,40 +89,58 @@
 	<?php echo $form->errorSummary($model); ?>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'username'); ?>
-		<?php echo $form->textField($model,'username',array('size'=>60,'maxlength'=>128)); ?>
-		<?php echo $form->error($model,'username'); ?>
-	</div>
+      <div class="col-25">
+        <label for="username">Username</label>
+      </div>
+      <div class="col-25">
+      <?php echo $form->textField($model,'username',array('size'=>60,'maxlength'=>15)); ?>
+      </div>
+    </div>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'password'); ?>
-		<?php echo $form->passwordField($model,'password',array('size'=>60,'maxlength'=>128)); ?>
-		<?php echo $form->error($model,'password'); ?>
-	</div>
+		<div class="col-25">
+			<label for="password">Password</label>
+		</div>
+		<div class="col-25">
+		<?php echo $form->passwordField($model,'password', array('size'=>60,'maxlength'=>255, 'class'=> 'form-control input-inline input-medium')); ?>
+		</div>
+		</div>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'email'); ?>
-		<?php echo $form->textField($model,'email',array('size'=>60,'maxlength'=>128)); ?>
-		<?php echo $form->error($model,'email'); ?>
-	</div>
+      <div class="col-25">
+        <label for="email">Email</label>
+      </div>
+      <div class="col-25">
+      <?php echo $form->textField($model,'email',array('size'=>60,'maxlength'=>15)); ?>
+      </div>
+    </div>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'first_name'); ?>
-		<?php echo $form->textField($model,'first_name',array('size'=>15,'maxlength'=>15)); ?>
-		<?php echo $form->error($model,'first_name'); ?>
-	</div>
+      <div class="col-25">
+        <label for="first_name">First Name</label>
+      </div>
+      <div class="col-25">
+      <?php echo $form->textField($model,'first_name',array('size'=>60,'maxlength'=>15)); ?>
+      </div>
+    </div>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'last_name'); ?>
-		<?php echo $form->textField($model,'last_name',array('size'=>15,'maxlength'=>15)); ?>
-		<?php echo $form->error($model,'last_name'); ?>
-	</div>
+      <div class="col-25">
+        <label for="last_name">Last Name</label>
+      </div>
+      <div class="col-25">
+      <?php echo $form->textField($model,'last_name',array('size'=>60,'maxlength'=>15)); ?>
+      </div>
+    </div>>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'roles'); ?>
-		<?php echo $form->textField($model,'roles',array('size'=>15,'maxlength'=>15)); ?>
-		<?php echo $form->error($model,'roles'); ?>
-	</div>
+      <div class="col-25">
+        <label for="rokes">Role</label>
+      </div>
+      <div class="col-25">
+      <?php echo $form->textField($model,'roles',array('size'=>60,'maxlength'=>15)); ?>
+      </div>
+    </div>
 
 	<div class="row buttons">
 		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
@@ -61,4 +148,9 @@
 
 <?php $this->endWidget(); ?>
 
-</div><!-- form -->
+  </form>
+</div>
+
+</body>
+</html>
+
